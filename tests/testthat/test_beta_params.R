29f44512eb95ba77d4b953cd9533e521cfeac263
Randall Pruim <rpruim@gmail.com>
2019-01-30 11:16:38 -0500

    betaParams() -> beta_params()

--- tests/testthat/test_beta_params.R
+++ tests/testthat/test_beta_params.R
@@ -1,13 +1,12 @@
 testthat::context("Checking zero_one conversions")
 
-test_that("betaPrams() calculates correctly.", {
+test_that("beta_params() calculates correctly.", {
   A <- dplyr::bind_rows(
-    betaParams(4, 6),
-    betaParams(shape1 = 4, shape2 = 6),
-    betaParams(40, 60),
-    betaParams(mean = 0.2, sd = 3),
-    betaParams(mode = 0.2, sd = 3),
-    betaParams(mode = 0.2, concentration = 8)
+    beta_params(4, 6),
+    beta_params(shape1 = 4, shape2 = 6),
+    beta_params(40, 60),
+    beta_params(mean = 0.2, sd = 3),
+    beta_params(mode = 0.2, concentration = 8)
   )
   expect_equivalent(A$mean, A$shape1 / (A$shape1 + A$shape2))
   expect_equivalent(A$mode, (A$shape1 - 1)  / (A$shape1 + A$shape2 - 2))
