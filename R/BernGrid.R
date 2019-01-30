713aeaafffa4b076008fdf773d01b9bad69d38b1
Randall Pruim <rpruim@gmail.com>
2019-01-30 11:15:23 -0500

    PropExample() -> BernGrid(); modify some defaults; allow prior to be a formula or function

--- R/BernGrid.R
+++ R/BernGrid.R
@@ -1,14 +1,18 @@
-#' Example: Bayesian inference of a proportion
+#' Bayesian inference for a proportion via grid method
 #'
 #' This intended as a black-box example with a small data set to illustrate
 #' the results of Bayesian updating.  The model is a Bernoulli model with
-#' fixed proportion over all tosses of the coin.  Prior, data, and grid resolution
+#' fixed proportion over all trials.  Prior, data, and grid resolution
 #' can be supplied by the user. See examples.
 #'
 #' @param data a vector of 0's and 1's or a character string with two characters representing
 #'   success/failure or head/tails from the bernoulli experiment.
 #' @param p a vector of probabilites to update.
-#' @param prior a vector of prior values at each probability in `p` or a constant.
+#' @param prior a vector of prior values at each probability in `p`,
+#'    a constant, a function, or a one-sided formula.
+#'    If a function or formula, [purrr::map_dbl()] is used to
+#'    apply to each value of `p`.  (See help for [purrr::map_dbl()]
+#'    for details regarding how this works.)
 #' @param results either `"plot"` or `"data"` to indicate what information is returned.
 #' @param steps a logical indicating wether the process should be shown step-by-step or
 #'   for all the data at once.
@@ -20,48 +24,61 @@
 #' @param alpha opacity used for the plot
 #' @param resolution if `p` is not specified, `resolution` an be used to create an equally
 #'   spaced grid of `resolution + 1` values from 0 to 1.
+#' @param filter an expression used to filter the results. See examples.
 #' @param ... additional arguments passed to the geom.
 #'
 #' @importFrom dplyr tibble filter
 #' @importFrom ggplot2 labs facet_wrap
+#' @importFrom purrr map_dbl
 #' @export
 #' @examples
-#' PropExample("HHTTH", geom = geom_col, p = seq(0, 1, by = 0.25), results = "data")
-#' PropExample("HHTTH",
-#'   p = seq(0, 1, by = 0.25), prior = c(.1, .2 , .4, .2, .1),
-#'   geom = geom_col, alpha = 0.5)
-#' PropExample("SSFFS", geom = geom_col, p = seq(0, 1, by = 0.25), steps = TRUE, results = "data")
-#' PropExample("SSFFS", p = seq(0, 1, by = 0.25), alpha = 0.9, steps = TRUE)
-#' PropExample("SSFFS", p = seq(0, 1, by = 0.25), alpha = 0.5, steps = TRUE,
-#'   geom = geom_col, filter = n %in% c(4,5))
-#' PropExample("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE)
-#' PropExample("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE)
-#' PropExample("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE, filter = n %in% c(1, 5, 10))
-#' PropExample("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE, filter = n == 10)
+#' BernGrid("HHTTH", p = seq(0, 1, by = 0.25), results = "data")
+#' BernGrid("HHTTH",
+#'             prior = ~ dbeta(.x, shape1 = 2, shape2 = 5))
+#' BernGrid("HHTTH", geom = geom_col, resolution = 100,
+#'             prior = ~ dbeta(.x, shape1 = 2, shape2 = 5))
+#' BernGrid("HHTTH", geom = geom_area, resolution = 100,
+#'             prior = ~ dbeta(.x, shape1 = 2, shape2 = 5))
+#' BernGrid("HHTTH",
+#'   p = seq(0, 1, by = 0.25), prior = c(.1, .2 , .4, .2, .1))
+#' BernGrid("SSFFS", p = seq(0, 1, by = 0.25), steps = TRUE, results = "data")
+#' BernGrid("SSFFS", p = seq(0, 1, by = 0.25), alpha = 0.9, steps = TRUE)
+#' BernGrid("SSFFS", p = seq(0, 1, by = 0.25), alpha = 0.5, steps = TRUE,
+#'   filter = n %in% c(4,5))
+#' BernGrid("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE)
+#' BernGrid("HHTTHTHHTHHH", steps = TRUE, geom = geom_area)
+#' BernGrid("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE, filter = n %in% c(1, 5, 10))
+#' BernGrid("HHTTHTHHTHHH", alpha = 0.9, steps = TRUE, filter = n == 10)
 #' # same posterior as in the previous example -- order doesn't matter
-#' PropExample("TTTTHHHHHHHH", alpha = 0.9, steps = TRUE, filter = n == 10)
-#' PropExample("HHHHHHHHHHHH", alpha = 0.9)
-#' PropExample("HHHHHHHHHHHH", alpha = 0.9, steps = TRUE)
-#' PropExample(rbinom(12, 1, 0.25), alpha = 0.9, steps = TRUE)
+#' BernGrid("TTTTHHHHHHHH", alpha = 0.9, steps = TRUE, filter = n == 10)
+#' BernGrid("HHHHHHHHHHHH", alpha = 0.9)
+#' BernGrid("HHHHHHHHHHHH", alpha = 0.9, steps = TRUE)
+#' BernGrid(rbinom(12, 1, 0.25), alpha = 0.9, steps = TRUE)
 
-PropExample <-
+BernGrid <-
   function(data = "HHTT",
            p = seq(0, 1, length.out = resolution + 1),
            prior = 1,
            results = c("plot", "data"),
            steps = FALSE,
-           geom = geom_line,
-           alpha = 0.7,
-           resolution = 100,
+           geom = if (length(p) <= 20) geom_col else geom_area,
+           alpha = 0.5,
+           resolution = 200,
            filter = TRUE,
            ...) {
-    results = match.arg(results)
-    probs = p  # force p to be evaluated in case it is determined by n
+    results <- match.arg(results)
+    probs <- p  # force p to be evaluated
+    if (is.function(prior) || inherits(prior, "formula")) {
+      prior <- purrr::map_dbl(probs, prior)
+    }
+
     filter <- enquo(filter)
+
+    converted <- FALSE
 
     if (is.factor(data)) {
       data <- as.numeric(data) - 1
-      message(paste("Converting data to", paste(data, collapse = ", ")))
+      converted <- TRUE
     }
 
     if (is.character(data)) {
@@ -74,7 +91,15 @@ PropExample <-
         # so S = 1 and F = 0, for example
         data <- as.numeric(factor(data)) - 1
       }
-      message(paste("Converting data to", paste(data, collapse = ", ")))
+      converted <- TRUE
+    }
+
+    if (converted) {
+      if (length(data) <= 20) {
+        message(paste("Converting data to", paste(data, collapse = ", ")))
+      } else {
+        message("Converting data to 0s and 1s")
+      }
     }
 
     if (! all(data %in% c(0,1))) {
@@ -84,7 +109,7 @@ PropExample <-
     if (steps) {
       Grid <-
         base::Reduce(c, data, accumulate = TRUE) %>%
-        base::lapply(PropExample, p = probs, results = "data", accumulate = TRUE) %>%
+        base::lapply(BernGrid, p = probs, results = "data", accumulate = TRUE) %>%
         dplyr::bind_rows() %>%
         dplyr::filter( !! filter) %>%
         group_by(p) %>%
@@ -136,7 +161,17 @@ PropExample <-
     } else {
       return(
         res +
-          ggplot2::labs(y = "", color = "", fill = "", title = paste(data, collapse = " "))
+          ggplot2::labs(y = "", color = "", fill = "",
+                        title =
+                          if (length(data) <= 16)
+                            paste(data, collapse = " ")
+                          else
+                            paste(
+                              length(data) - sum(data), " + " ,
+                              sum(data), " = " ,
+                              length(data))
+                        )
+
       )
     }
   }
