c0f9fc60fca82c27ca68c508800f97974565391d
Randall Pruim <rpruim@gmail.com>
2019-02-19 19:12:29 -0500

    add quick_bern_beta()

---
+++ R/quick-bern-beta.R
@@ -0,0 +1,45 @@
+
+#' Quick determination of posterior from a beta prior and Bernoulli likelihood
+#'
+#' Quick determination of posterior from a beta prior and Bernoulli likelihood
+#'
+#'
+#' @export
+#' @param x number of successes in data
+#' @param n number of trials in data
+#' @param ... prameters describing the beta prior.  Uses [beta_params()].
+#'
+quick_bern_beta <-
+  function(
+    x, n,     # data, successes and trials
+    ...       # see clever trick below
+  )
+  {
+    pars <- beta_params(...)
+    a <- pars$shape1
+    b <- pars$shape2
+
+    theta_hat <- x / n  # value that makes likelihood largest
+    posterior_mode <- (a + x - 1) / (a + b + n - 2)
+
+    # scale likelihood to be as tall as the posterior
+    likelihood <- function(theta) {
+      dbinom(x, n, theta) / dbinom(x, n, theta_hat) *
+        dbeta(posterior_mode, a + x, b + n - x)  # posterior height at mode
+    }
+
+    gf_dist("beta", shape1 = a, shape2 = b,
+            color = ~ "prior", alpha = 0.5, xlim = c(0,1), size = 1.2) %>%
+      gf_function(likelihood,
+                  color = ~ "likelihood", alpha = 0.5, size = 1.2) %>%
+      gf_dist("beta", shape1 = a + x, shape2 = b + n - x,
+              color = ~ "posterior", alpha = 0.5, size = 1.6) %>%
+      gf_labs(
+        color = "function",
+        title = paste0("posterior: Beta(", a + x, ", ", b + n - x, ")")
+      ) %>%
+      gf_refine(
+        scale_color_manual(
+          values = c("prior" = "gray50", "likelihood" = "forestgreen",
+                     "posterior" = "steelblue")))
+  }
